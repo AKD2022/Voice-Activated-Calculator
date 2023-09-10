ba8527599529035a6baa5504c86b343126cf0f4f
AKD <akdusa20@gmail.com>
2023-09-10 10:24:46 -0400

    commit

--- AndroidApp/app/src/main/java/com/example/myapplication/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,7 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //IDs of all the numeric buttons
+    private int[]
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
